caa0306e289f7ded734ce0e311254175fc0e25ff
notluckycharm <70812022+notluckycharm@users.noreply.github.com>
2025-01-28 15:20:58 -0500

    Learning View in progress

---
+++ AlabamaDictionaryMobile/LearnView.swift
@@ -0,0 +1,18 @@
+//
+//  LearnView.swift
+//  AlabamaDictionaryMobile
+//
+//  Created by Jacob Fernandes on 1/21/25.
+//
+
+import SwiftUI
+
+struct LearnView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LearnView()
+}
